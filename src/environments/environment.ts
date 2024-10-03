b7a45435d78b657c2460d6d7383c4288e9c20d78
Mihai <vescmihai@gmail.com>
2024-10-03 12:48:04 -0400

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
-  wsUrl: 'http://localhost:3000',
+  apiUrl: 'https://software1-uml-b2a81ff8ce30.herokuapp.com',
+  wsUrl: 'https://software1-uml-b2a81ff8ce30.herokuapp.com',
 };
